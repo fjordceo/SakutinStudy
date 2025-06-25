36a214f914a971b784c3570b9d6cf1c1f99b49db
Mikhail Lisaev <fjordceo@hotmail.com>
2025-06-25 14:55:07 +0700

    Lesson 001 is started

--- Episode001_Variables/Program.cs
+++ Episode001_Variables/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Второй цикл уроков для запоминания и повторения!");
         }
     }
 }
