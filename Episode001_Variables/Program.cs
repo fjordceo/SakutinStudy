3a90da3726519b2e80cd44e50fcb4b7d072b3964
Mikhail Lisaev <fjordceo@hotmail.com>
2025-06-25 14:53:42 +0700

    Добавьте файлы проекта.

---
+++ Episode001_Variables/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Episode001_Variables
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
