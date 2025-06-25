b91a6d4b5ca47221f974b25c9f1c0372722af283
Mikhail Lisaev <fjordceo@hotmail.com>
2025-06-25 16:19:21 +0700

    Lesson 002 is completed

--- Episode002_ArithmeticOperations/Program.cs
+++ Episode002_ArithmeticOperations/Program.cs
@@ -6,7 +6,7 @@
 
 
 
-{
+{ 
     internal class Program
     {
         static void Main(string[] args)
@@ -18,6 +18,25 @@
             // Деление /
             // Целочисленное деление //
             // Остаток от деления (Деление по модулю) %
+
+            float result, modulResult;
+            int a, b;
+
+            a = 10;
+            b = 3;
+            result = (float)a / b;  // Приведение типов для получения дробного результата
+            modulResult = a % b; // Остаток от деления
+            result++; // Увеличиваем результат на 1
+            result *= 2; // Умножаем результат на 2
+
+
+
+
+            Console.WriteLine($"Переменная a = {a}");
+            Console.WriteLine($"Переменная b = {b}");
+            Console.WriteLine($"Результат деления {result}");
+            Console.WriteLine($"Результат деления по модулю {modulResult}");
+
 
 
 
