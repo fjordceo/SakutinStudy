81686f1c96e75861939565185f85d97745d1a882
Mikhail Lisaev <fjordceo@hotmail.com>
2025-07-04 15:43:11 +0700

    Lesson 019 is completed

---
+++ Episode018_Random/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Episode018_Random
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random(); // Создаем экземпляр класса Random
+
+            int randomNumber; // Генерируем случайное число
+
+            int randomCounter = 0;
+
+            while (randomCounter < 100)
+            { 
+                randomNumber = random.Next(1, 101);
+                Console.WriteLine($"Итерация {randomCounter+1}. Случайное число: {randomNumber}");
+                Thread.Sleep(500);
+                randomCounter++;
+            }
+        }
+    }
+}
---
+++ Episode019_PracticeGuessTheNumber/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace Episode019_PracticeGuessTheNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int userGuess;
+            int triesCount = 5;
+
+            while (triesCount > 0)
+            {
+                triesCount--;
+                Console.Write("Угадайте число от 1 до 10: ");
+                string? input = Console.ReadLine();
+                if (int.TryParse(input, out userGuess))
+                {
+
+                    int randomNumber = random.Next(1, 11);
+                    if (userGuess < 1 || userGuess > 10)
+                    {
+                        Console.WriteLine("Пожалуйста, введите число от 1 до 10.");
+                        continue;
+                    }
+                    if (userGuess == randomNumber)
+                    {
+                        Console.WriteLine("Поздравляем! Вы угадали число!");
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Неправильно. Попробуйте еще раз.");
+                        Console.WriteLine($"У вас осталось {triesCount} попыток");
+                        Console.WriteLine($"Загаданное число было: {randomNumber}");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Пожалуйста, введите корректное число.");
+                }
+            }
+        }
+    }
+}
+
+// 2 часа 21 минута
